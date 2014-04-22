fddd14d423b5e249004baab6a699ad7cd872e1fb
Marcus Willock <crazcalm@gmail.com>
2014-04-22 07:55:29 -0400

    Started writing notes on the grid geometry manager

---
+++ Tk_practice/Basics/learn_grid.py
@@ -0,0 +1,10 @@
+"""
+Grid Geometry Manager:
+----------------------
+
+    The central idea of the grid geometry manager is to divide the container
+frame into a two-dimentional table divided into a number of rows and columns.
+
+    Each cell in the table can be
+
+"""
