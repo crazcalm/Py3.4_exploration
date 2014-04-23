03e4cc52f9ebd48f0de6f504a4519230a2af8aec
Marcus Willock <crazcalm@gmail.com>
2014-04-23 07:49:15 -0400

    reviewing recursion

---
+++ DataStructures/Recursion/examples.py
@@ -0,0 +1,12 @@
+"""
+Recursion:
+----------
+
+    Recursion is a method of solving problems that invloves breaking
+problems down into smaller and smaller subproblems until you get to a small
+enough problem that it can solves trivially.
+
+The Three Laws of Recursion:
+
+write later
+"""
