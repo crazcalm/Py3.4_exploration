a4d4a773016a62fb82bde8e0688deb668cbd672b
Marcus Willock <crazcalm@gmail.com>
2014-04-23 20:30:19 -0400

    two examples done

--- DataStructures/Recursion/examples.py
+++ DataStructures/Recursion/examples.py
@@ -8,5 +8,23 @@ enough problem that it can solves trivially.
 
 The Three Laws of Recursion:
 
-write later
+1. A recursive algorithm must have a base case.
+
+2. A recursive algorithm must change its state and move towards the base case.
+
+3. A recursive algorithm must call itself, recursively.
 """
+
+def listsum(numList):
+    if len(numList) == 1:
+        return numList[0]
+    else:
+        return numList[0] + listsum(numList[1:])
+
+def toStr(n, base):
+    convertString = "0123456789ABCDEF"
+
+    if n < base:
+        return convertString[n]
+    else:
+        return toStr(n//base, base) + convertString[n%base]
