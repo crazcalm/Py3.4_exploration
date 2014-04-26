361117112e5347b99e4c999f2f7b366c58da65b9
Marcus Willock <crazcalm@gmail.com>
2014-04-26 14:47:33 -0400

    Some notes on the dialogs

---
+++ Tk_practice/Basics/learn_forms_and_dialogs.py
@@ -0,0 +1,27 @@
+"""
+                tkFileDialog:
+                ---------------------
+
+WARNING:
+--------
+
+The tkFileDialog has been repackaged in python 3 to tkinter.filedialog.
+
+python 2 ---> import tkFileDialog
+
+python 3 ---> from tkinter import filedialog (or some other flavor of import)
+
+
+
+Some Dialogs:
+-------------
+
+There are a lot of them. Look up when needed.
+
+Notes:
+------
+
+The Documentation seems pretty straight forward. Read them!
+
+Also, These widgets are ugly!!!!
+"""
