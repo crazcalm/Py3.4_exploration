375a5dd2b1d228ca2cb51105d14ef93d55a760fd
Marcus Willock <crazcalm@gmail.com>
2014-04-26 15:16:04 -0400

    Some note on message boxes

---
+++ Tk_practice/Basics/learn_message_boxes.py
@@ -0,0 +1,31 @@
+"""
+Notes:
+------
+
+python3 --> repackaged it as tkinter.messagebox.
+
+See docs
+
+Some messageboxes:
+-------------
+
+showwarning
+showinfo
+showerror
+askquestion
+askokcancel
+askyesno
+askretrycancel
+
+
+Cool feature!
+-------------
+
+These boxes take two inputs.
+
+1. A string that maps to a img(or sign)
+
+2. The string that will be shown.
+"""
+
+
