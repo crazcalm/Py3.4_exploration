44e52e9804c74892b343c8fe7402d050169297a1
Marcus Willock <crazcalm@gmail.com>
2014-04-28 07:41:28 -0400

    I should have never wrote this out. It has too many assumptions, it will take me FOREVER to debug and get running. Do it later

--- DataStructures/Recursion/maze.py
+++ DataStructures/Recursion/maze.py
@@ -39,7 +39,60 @@ class Maze:
             for x in range(self.columnInMaze):
                 if self.mazelist[y][x] == OBSTACLE:
                     self.drawCenterBox(x+self.xTranslate,
-                                       -y)
+                                       -y+ self.yTranslate,
+                                            "tan")
+
+        self.t.color('black', 'blue')
+
+    def drawCenteredBox(self, x, y, color):
+        tracer(0)
+        self.t.up()
+        self.t.goto(x-.5, y-.5)
+        self.t.color("black", color)
+        self.t.setheading(90)
+        self.t.down()
+        self.begin_fill()
+        for i in range(4):
+            self.t.forward(1)
+            self.t.right(90)
+        self.end_fill()
+        update()
+        tracer(1)
+
+    def moveTurtle(self, x, y):
+        self.t.up()
+        self.t.setheading(self.t.towards(x+self.xTranslate,
+                -y + self.yTranslate))
+        self.t.goto(x + self.xTranslate, -y+self.yTranslate)
+
+    def dropBreadcrumb(self, color):
+        self.t.dot(color)
+
+    def updatePosition(self, row, col, val=None):
+        if val:
+            self.mazelist[row][col] = val
+        self.moveTurtle(col. row)
+
+        if val == PART_OF_PATH:
+            color = 'green'
+        elif val == OBSTACLE:
+            color = 'red'
+        elif val == TRIED:
+            color = 'black'
+        elif val == Dead_END:
+            color = 'red'
+        else:
+            color = None
+
+        if color:
+            self.dropBreadcrumb(color)
+
+    def isExit(self, row, col):
+        return (row == 0 or row == self.rowsInMaze-1
+                    or col == 0, col == self.columnInMaze-1)
+
+    def __getitem__(self, idx):
+        return self.mazelist[idx]
 
 
 
