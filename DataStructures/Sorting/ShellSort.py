a57391a61121f4b2f0eadba6b408bd86e9bb1e05
Marcus Willock <crazcalm@gmail.com>
2014-04-29 22:20:00 -0400

    I now have Shell sort

--- DataStructures/Sorting/ShellSort.py
+++ DataStructures/Sorting/ShellSort.py
@@ -18,5 +18,49 @@ General Idea:
 -------------
 
     You sort the sublists, and then you sort the entire list. The sorted
+sublist help in reducing the total amout of passes of the shell Sort.
 
+Big O:
+------
+
+O(n^(3/2))
 """
+from BubbleSort import randomList
+from InsertionSort import insertionSort
+
+def shellSort(alist):
+    """
+    A basic Shell Sort
+    """
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+
+        #print("After increments of size", sublistcount,
+         #       "The list is", alist ,"\n")
+
+        sublistcount = sublistcount//2
+
+    return insertionSort(alist)
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start + gap, len(alist), gap):
+
+        currentvalue = alist[i]
+        position = 1
+
+        while position >= gap and \
+                alist[position-gap] > currentvalue:
+            alist[position] = alist[position - gap]
+            position = position - gap
+
+        alist[position] = currentvalue
+
+    return alist
+
+if __name__ == "__main__":
+    test = randomList(20)
+    print(test, "\n")
+    print(shellSort(test))
