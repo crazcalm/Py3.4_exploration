95ae571676cf7a8a10b92fe80730518215a1daa8
Marcus Willock <crazcalm@gmail.com>
2014-04-30 08:34:35 -0400

    QuickSort finished before I arrivedgit add QuickSort.pygit add QuickSort.py YES

--- DataStructures/Sorting/QuickSort.py
+++ DataStructures/Sorting/QuickSort.py
@@ -67,31 +67,51 @@ If the split occurs in the middle of the list, then O(nlogn).
 Else, O(n^2)
 """
 
+from BubbleSort import randomList
 
+def quickSort(alist):
+    quickSortHelper(alist, 0, len(alist)-1)
+    return alist
 
+def quickSortHelper(alist, first, last):
+    if first < last:
+        splitpoint = partition(alist, first, last)
 
+        quickSortHelper(alist, first, splitpoint-1)
+        quickSortHelper(alist, splitpoint+1, last)
 
+def partition(alist, first, last):
+    pivotvalue = alist[first]
 
+    leftmark = first + 1
+    rightmark = last
 
+    done = False
 
+    while not done:
 
+        while leftmark <= rightmark and \
+                    alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
 
+        while alist[rightmark] >= pivotvalue and \
+                    rightmark >= leftmark:
+            rightmark = rightmark -1
 
+        if rightmark < leftmark:
+            done = True
+        else:
+            tempt = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = tempt
 
+    tempt = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = tempt
 
+    return rightmark
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    test = randomList(20)
+    print(test)
+    print(quickSort(test))
