6bd9c7325691345a14fdc181de63831780f1061a
Marcus Willock <crazcalm@gmail.com>
2014-05-22 02:18:21 -0400

    I now have a infix to postfix converter

--- DataStructures/Stack/InfixToPostfix.py
+++ DataStructures/Stack/InfixToPostfix.py
@@ -25,5 +25,46 @@ The addition operator then appears before the A and the result of the multiplica
 is preserved since the * appears immediately after the B and C, denoting that
 * has precedence, with + coming after.
 
-
 """
+
+from stack import Stack
+import string
+
+def infixToPostfix(infixexpr):
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec['-'] = 2
+    prec['('] = 1
+
+    opStack = Stack()
+    postfixList = []
+
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in string.ascii_uppercase:
+            postfixList.append(token)
+        elif token == "(":
+            opStack.push(token)
+        elif token == ")":
+            topToken = opStack.pop()
+            while topToken != "(":
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and \
+              (prec[opStack.peek()] >= prec[token]):
+                postfixList.append(opStack.pop())
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+
+    return " ".join(postfixList)
+
+if __name__ == "__main__":
+    print(infixToPostfix("( A + B ) * ( C + D )"))
+    print(infixToPostfix("( A + B ) * C"))
+    print(infixToPostfix("A + B * C"))
