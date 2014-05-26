77d5f6afd6abf807c4ba8b8d0a3dc81e006edb9c
Marcus Willock <crazcalm@gmail.com>
2014-05-26 10:07:45 -0400

    Notes are done. Time to start writing the code

--- DataStructures/Trees/PriorityQueue.py
+++ DataStructures/Trees/PriorityQueue.py
@@ -37,6 +37,60 @@ Basic Operations List:
   delMin(): returns the item with the minimum key value, removing the item from the list.
 
   isEmpty(): returns true if the heap is empty, false otherwise.
+
+  size(): returns the number of items in the heap.
+
+  buildHeap(list): builds a new heap from a list of keys.
+
+
+The Structure Property:
+-----------------------
+
+  In order to make our heap work effeciently, we will take advantage of the
+logarithmic nature of the tree to represent our heap.
+
+  In order to guarantee logarithmic performance, we must keep our tree balanced.
+A balanced binary tree has roughly the same number of nodes in the left and right
+subtrees of the root.
+
+  In our heap implemention we keep the tree balanced by creating a 'complete
+binary tree.'
+
+  A complete binary tree is a tree in which each level has all of its nodes.
+The exception to this is the bottom of the tree, which we fill in from left to
+right.
+
+
+Interesting Property:
+---------------------
+
+  Another interesting property of a complete tree is that we can represent it
+using a single list. We do not need to use nodes and references or even lists
+of lists.
+
+  Because the tree is complete, the left child of the parent (at position p) is
+is the node that is found a position 2p in the list. Similarly, the right child
+of the parent is at position 2p+1 in the list.
+
+
+The Heap Order Property:
+------------------------
+
+  The 'heap order property' is as follows: In a heap, for every node x with
+parent p, the key in p is smaller than or equal to the key in x.
+
+
+Heap Operations:
+----------------
+
+  We will begin our implemention of a binary heap with the constructor.
+Since the entire binary heap can be represented by a single list, all the constructor
+will do is initialize the list and an attribute currentSize to keep track of the
+current size of the heap.
+
+  You will notice that an empty binary heap has a single zero as the first element
+of heapList and that this zero is not used, but is there so that a simple
+integer can be used in later methods.
 """
 
 
