5f4cbc8e966217f0ab91cbc782e3a2c016c1eb0f
Marcus Willock <crazcalm@gmail.com>
2014-05-27 17:34:06 -0400

    Wrote out most of the TreeNode class and added the put and _put methods to the BinarySearchTree class

--- DataStructures/Trees/BinarySearchTree.py
+++ DataStructures/Trees/BinarySearchTree.py
@@ -121,6 +121,50 @@ _put to search the tree according to the following algorithm.
     object at the point discovered by the previous step.
 """
 
+class TreeNode:
+
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+    def isLeaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasbothChildren(self):
+        return self.rightChild and self.leftChild
+
+    def replaceNodeDatat(self, key, value, lc, rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+
 class BinarySearchTree:
 
     def __init_(self):
@@ -129,6 +173,28 @@ class BinarySearchTree:
 
     def length(self):
         return self.size
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self, val)
+        else:
+            self.root = TreeNode(key, val)
+        self.size = size + 1
+
+    def _put(self, key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key, value,
+                                                parent = currentNode)
+
+        else:
+            if currentNode.hasRightChild():
+                self._put(key, value, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key, value,
+                                                parent = currentNode)
 
     def __len__(self):
         return self.size
