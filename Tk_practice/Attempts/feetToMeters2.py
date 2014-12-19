ce4dc0444c978d0302e44f808f4c30d9284a89a7
crazcalm <crazcalm@gmail.com>
2014-12-19 02:12:11 -0500

    I made the tutorial into a class, but I cannot get the bind method to work

---
+++ Tk_practice/Attempts/feetToMeters2.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import ttk
+
+class FeetToMeters(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        print(dir(self))
+        self.mainframe = ttk.Frame(self, padding="3 3 12 12")
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.mainframe.rowconfigure(0, weight=1)
+
+        self.feet = StringVar()
+        self.meters = StringVar()
+
+        self.feet_entry = ttk.Entry(self.mainframe, width=7,
+                textvariable=self.feet)
+        self.feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+        ttk.Label(self.mainframe, textvariable=self.meters).grid(column=2,
+                row=2, sticky=(W, E))
+        ttk.Button(self.mainframe, text="Calculate",
+                command=self.calculate).grid(column=3, row=3, sticky=W)
+
+        ttk.Label(self.mainframe, text='feet').grid(column=3, row=1,
+                sticky=W)
+        ttk.Label(self.mainframe, text='is equivalent to').grid(column=1,
+                row=2, sticky=E)
+        ttk.Label(self.mainframe, text='meters').grid(column=3, row=2,
+                sticky=W)
+
+        for child in self.mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+        self.feet_entry.focus()
+        self.bind('<Return>', self.calculate)
+
+        self.pack()
+    def calculate(self):
+        try:
+            value = float(self.feet.get())
+            self.meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+        except ValueError:
+            pass
+
+if __name__ == "__main__":
+    root = Tk()
+    test = FeetToMeters(root)
+    test.mainloop()
